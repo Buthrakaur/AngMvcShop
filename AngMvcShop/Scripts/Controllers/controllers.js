a0c2b79fe72e7dc755d69f6b85ef9d1113785537
Filip Kinský <filip@filovo.net>
2013-04-07 00:29:03 +0200

    rozchozeny dotaz na server

--- AngMvcShop/Scripts/Controllers/controllers.js
+++ AngMvcShop/Scripts/Controllers/controllers.js
@@ -1,16 +1,11 @@
-var Product = (function () {
-    function Product(Name, ThumbnailUrl) {
-        this.Name = Name;
-        this.ThumbnailUrl = ThumbnailUrl;
-    }
-    return Product;
-})();
 function ProductListController($scope, $http) {
     $scope.pageSize = 10;
     $scope.currentPage = 0;
-    $scope.products = [
-        new Product('Produkt 1', 'http://placehold.it/50x50&text=1'), 
-        new Product('Produkt 2', 'http://placehold.it/50x50&text=2'), 
-        
-    ];
+    $scope.products = [];
+    $http.post('/Home/GetProductsPage', {
+        offset: $scope.currentPage,
+        count: $scope.pageSize
+    }).success(function (data) {
+        return $scope.products = data;
+    });
 }
--- AngMvcShop/Scripts/Controllers/controllers.ts
+++ AngMvcShop/Scripts/Controllers/controllers.ts
@@ -1,16 +1,15 @@
 /// <reference path="../typings/angularjs/angular.d.ts" />
 /// <reference path="../typings/angularjs/angular-resource.d.ts" />
 
-class Product{
-    constructor (public Name: string, public ThumbnailUrl: string){
-    }
+interface Product{
+    Name: string;
+    ThumbnailUrl: string;
 }
 
 function ProductListController($scope, $http: ng.IHttpService) {
     $scope.pageSize = 10;
     $scope.currentPage = 0;
-    $scope.products = [
-        new Product('Produkt 1', 'http://placehold.it/50x50&text=1'),
-        new Product('Produkt 2', 'http://placehold.it/50x50&text=2'),
-        ];
+    $scope.products = [];
+    $http.post('/Home/GetProductsPage', { offset: $scope.currentPage, count: $scope.pageSize })
+        .success(data => $scope.products = <Product[]>data);
 }
